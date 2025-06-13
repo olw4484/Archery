b18331a30514dbe8a3d7a8ceb1ef5713f729880d
owl4484 <rodeo23267286@gmail.com>
2025-06-13 21:45:21 +0900

    [Fix] Detach controller from HMD for proper bow pulling space
    - CharacterController 비활성화로 XR Origin 이동 억제
    - 컨트롤러가 HMD에 고정되지 않고 독립적으로 추적되도록 수정
    - 활 시위를 당길 수 있는 실제 거리 확보

--- Assets/Script/Arrow/ArrowBucket.cs
+++ Assets/Script/Arrow/ArrowBucket.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class ArrowBucket : MonoBehaviour
 {
@@ -39,27 +40,34 @@ public class ArrowBucket : MonoBehaviour
             Destroy(arrow.gameObject);
         }
 
-        arrowsRemaining = totalArrowCount;
+        StartCoroutine(SpawnArrowsSequentially());
+    }
 
+    private IEnumerator SpawnArrowsSequentially()
+    {
+        arrowsRemaining = totalArrowCount;
 
         for (int i = 0; i < totalArrowCount; i++)
         {
             GameObject newArrow = Instantiate(arrowPrefab, spawnRoot);
-
-            
-            Vector3 spawnPos = Vector3.up * (i * 0.12f); 
+            Vector3 spawnPos = Vector3.up * (i * 0.12f - 0.25f); // 높이 및 보정
             newArrow.transform.localPosition = spawnPos;
-            newArrow.transform.localRotation = Quaternion.identity;
+            newArrow.transform.localRotation = Quaternion.Euler(90, 0, 0); // 필요 시 각도 조정
 
             Arrow arrow = newArrow.GetComponent<Arrow>();
             if (arrow != null)
             {
                 arrow.OnTaken += OnArrowTaken;
             }
+
+            UIManager.Instance?.UpdateArrowCount(arrowsRemaining);
+
+            yield return new WaitForSeconds(0.05f); // 너무 느리면 0.03f 정도로
         }
 
-        UIManager.Instance?.UpdateArrowCount(arrowsRemaining);
+        Debug.Log("[ArrowBucket] All arrows spawned sequentially.");
     }
+
 
 
     public int GetArrowCount() => arrowsRemaining;
